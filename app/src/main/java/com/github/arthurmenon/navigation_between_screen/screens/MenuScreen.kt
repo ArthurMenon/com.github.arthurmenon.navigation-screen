18ea4b1d3f3c1a509f782162912ce9274230f4bf
ArthurMenon <202712134+ArthurMenon@users.noreply.github.com>
2026-03-17 18:06:30 -0300

    Implementa navegação entre telas com Navigation Compose, passando NavController e innerPadding para cada tela via MainActivity

--- app/src/main/java/com/github/arthurmenon/navigation_between_screen/screens/MenuScreen.kt
+++ app/src/main/java/com/github/arthurmenon/navigation_between_screen/screens/MenuScreen.kt
@@ -19,10 +19,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 
 @Composable
-fun MenuScreen(modifier: Modifier = Modifier) {
-    Box(
+fun MenuScreen(modifier: Modifier = Modifier, navController: NavController) {    Box(
         modifier = modifier
             .fillMaxSize()
             .background(Color(0xFF2C4EC7))
@@ -41,7 +41,7 @@ fun MenuScreen(modifier: Modifier = Modifier) {
                 .align(Alignment.Center)
         ) {
             Button(
-                onClick = { /*TODO*/ },
+                onClick = { navController.navigate("perfil") },
                 colors = ButtonDefaults.buttonColors(Color.White),
                 modifier = Modifier.size(width = 200.dp, height = 48.dp)
             ) {
@@ -53,7 +53,7 @@ fun MenuScreen(modifier: Modifier = Modifier) {
             }
             Spacer(modifier = Modifier.height(16.dp))
             Button(
-                onClick = { /*TODO*/ },
+                onClick = { navController.navigate("pedidos") },
                 colors = ButtonDefaults.buttonColors(Color.White),
                 modifier = Modifier.size(width = 200.dp, height = 48.dp)
             ) {
@@ -65,7 +65,7 @@ fun MenuScreen(modifier: Modifier = Modifier) {
             }
             Spacer(modifier = Modifier.height(16.dp))
             Button(
-                onClick = { /*TODO*/ },
+                onClick = { navController.navigate("login") },
                 colors = ButtonDefaults.buttonColors(Color.White),
                 modifier = Modifier.size(width = 200.dp, height = 48.dp)
             ) {
